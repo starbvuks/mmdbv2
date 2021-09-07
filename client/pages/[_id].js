2c4b13ff64f7cf6dd855cda7c640df70d35d5838
Sarvag Kalari <starbvuks@gmail.com>
2021-09-08 00:25:23 +0530

    pre ssg

--- client/pages/[_id].js
+++ client/pages/[_id].js
@@ -2,38 +2,38 @@ import React from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
 
-// export const getStaticPaths = async () => {
-//   const res = await axios.get("http://localhost:451/movies");
+export const getStaticPaths = async () => {
+  const res = await axios.get("http://localhost:451/movies");
+  const data = res.data;
+  const paths = data.map((movie) => {
+    return {
+      params: { _id: movie._id.toString() },
+    };
+  });
 
-//   const paths = res.map((movie) => {
-//     return {
-//       params: { id: movie._id.toString() },
-//     };
-//   });
-
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// };
-
-export const getStaticProps = async () => {
-  const router = useRouter();
-  const id = router.query._id;
-  const res = axios.get("http://localhost:451/movies/" + id);
-  const data = await res.json().data();
   return {
-    props: { movie: data },
+    paths,
+    fallback: false,
   };
 };
 
-const MoviePage = () => {
+// export const getStaticProps = async (context) => {
+//   const id = context.params.id;
+//   const res = await axios.get("http://localhost:451/movies/" + id);
+//   const data = JSON.stringify(res);
+
+//   return {
+//     props: data,
+//   };
+// };
+
+const MoviePage = ({ data }) => {
   const router = useRouter();
   console.log(router.query._id);
 
   return (
     <div>
-      <span>yo</span>
+      <span>{res}</span>
     </div>
   );
 };
