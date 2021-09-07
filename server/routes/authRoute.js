ef63d35d168d2144346e47fb3850a93ccce63c5b
Sarvag Kalari <starbvuks@gmail.com>
2021-09-07 14:57:57 +0530

    joi validation

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,9 +1,19 @@
 const router = require("express").Router();
 const User = require("../models/userModel");
+const Joi = require("joi");
+
+const schema = Joi.object({
+  email: Joi.string().email().required(),
+  username: Joi.string().required(),
+  password: Joi.string().required(),
+});
 
 router.post("/register", async (req, res) => {
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   const user = new User({
-    name: req.body.name,
+    email: req.body.name,
     username: req.body.username,
     password: req.body.password,
   });
